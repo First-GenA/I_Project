05c496564720e77f064c848251a56fd0c83151d4
Fist-GenA <andy.nlarry@gmail.com>
2025-03-03 15:00:32 +0300

    edited settings file

--- personalWeb/personalWeb/settings.py
+++ personalWeb/personalWeb/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # installed apps
-    'personalWeb.personalWeb.personal',
+    'personal',
 ]
 
 MIDDLEWARE = [
