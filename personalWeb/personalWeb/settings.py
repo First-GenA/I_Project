b5d5f7d81af7490e0489ae8f0de93b5b172563c7
Fist-GenA <andy.nlarry@gmail.com>
2025-03-03 15:54:59 +0300

    edited settings file

--- personalWeb/personalWeb/settings.py
+++ personalWeb/personalWeb/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')# 
 # Application definition
 
 INSTALLED_APPS = [
+    'personal.apps.PersonalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # installed apps
-    'personal.apps.PersonalConfig',
+    # 'personal.apps.PersonalConfig',
 ]
 
 MIDDLEWARE = [
